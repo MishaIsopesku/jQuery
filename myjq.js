33d08ea5f190edb43f5794ebc600666df809ed46
Misha Isopesku <isopesku05@gmail.com>
2022-09-08 16:28:11 +0300

    first commit

---
+++ myjq.js
@@ -0,0 +1,127 @@
+ /*------------THEME 1------------*/
+ /*1.1*/ 
+ $(function (){
+  $(document).ready(function () {
+    $('#btn1').click(function(){
+      var html = $('h1').text();
+      alert(html);
+    });
+});
+});
+ /*1.1 END */ 
+ 
+  /*1.2 */
+  $(function (){
+    $(document).ready(function () {
+      $('#btn2').click(function(){
+        var html = $('.link-moto').text();
+        alert(html);
+      });
+  });
+  });
+  /*1.2 END */ 
+
+  /*1.3 */
+  $(function (){
+    $('#btn3').click(function() {
+      $('.slide-btn').hide();
+  });
+  });
+  /*1.3 END */
+
+  /*1.4 */ 
+  $(function (){
+    $('#btn4').click(function() {
+      $('.features').hide(3000);
+  });
+  });
+  /*1.4 END */
+  
+  /*1.5 */
+  $(function (){
+    $('#btn5').click(function() {
+      $('.categorie').show(2000);
+      
+  });
+  }); 
+  /*1.5 END */
+
+  /*1.6*/ 
+  $(function (){
+    $(document).ready(function () {
+      $('#btn6').click(function(){
+        var html = $('.s').text();
+        alert(html);
+      });
+  });
+  });
+  /*1.6 END */
+
+  /*1.7 */
+  $(function (){
+    $('#btn7').click(function() {
+      $('h2').hide(3500);
+    $('#btn7').click(function() {
+        $('h2').show(2000);
+      }); 
+      
+  });
+  }); 
+  /*1.7 END */
+
+  /*1.8 */
+  $(function (){
+    $('#btn8').click(function() {
+      $('.pin').hide(1000);
+    $('#btn8').click(function() {
+        $('.pin').show(2000);
+      }); 
+      
+  });
+  }); 
+  /*1.8 END */
+
+  /*1.9 */
+  $(function (){
+    $(document).ready(function () {
+      $('#btn9').click(function(){
+        var html = $('.price').text();
+        alert(html);
+      });
+  });
+  });
+  /*1.9 END */
+
+  /*1.10 */
+  $(function (){
+    $(document).ready(function () {
+      $('#btn10').click(function(){
+        var html = $('.num').text();
+        alert(html);
+      });
+  });
+  });
+  /*1.10 END */
+
+  /*1.11 */
+  $(function (){
+    $(document).ready(function () {
+      $('#btn11').click(function(){
+        var html = $('.post-head').text();
+        alert(html);
+      });
+  });
+  });
+  /*1.11 END */
+
+  /*1.12 */ 
+  $(function (){
+    $('#btn12').click(function() {
+      $('.hideimg').hide();
+  });
+  });
+  /*1.12 END */
+  /*----------THEME 1 END----------*/
+
+
+  
